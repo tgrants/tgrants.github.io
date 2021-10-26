9af72233a46e6172c4b6cee8788f4fa4ea2cc2eb
klappscheinwerfer <75024514+klappscheinwerfer@users.noreply.github.com>
2021-10-26 08:29:30 +0300

    Add event listener

--- js/loadXML.js
+++ js/loadXML.js
@@ -11,3 +11,6 @@ function loadXMLDoc(url) {
 	xmlhttp.send(null);
 	document.getElementById('main').innerHTML=xmlhttp.responseText;
 }
+document.addEventListener("DOMContentLoaded", loadXMLDoc('projects.xml'){
+    console.log('DOM content loaded');
+})
