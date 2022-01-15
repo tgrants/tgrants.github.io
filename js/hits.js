a32a9ed1b56adddc746a88fccb6c8ed95de18570
klappscheinwerfer <75024514+klappscheinwerfer@users.noreply.github.com>
2022-01-15 16:13:01 +0200

    Analytics and enable hits

--- js/hits.js
+++ js/hits.js
@@ -10,9 +10,9 @@ function getHitCount(key, classname) {
 
 window.addEventListener('DOMContentLoaded', (event) => {
     today = new Date;
-    //getHitCount("total", "hits-total");
+    getHitCount("total", "hits-total");
     // Could break if year is less than 100 because key needs to be at least 3 characters long
-    //getHitCount("Y" + today.getFullYear(), "hits-yearly");
-    //getHitCount("Y" + today.getFullYear() + "M" + today.getMonth(), "hits-monthly");
-    //getHitCount("Y" + today.getFullYear() + "M" + today.getMonth() + "daily" + today.getDate(), "hits-daily");
+    getHitCount("Y" + today.getFullYear(), "hits-yearly");
+    getHitCount("Y" + today.getFullYear() + "M" + today.getMonth(), "hits-monthly");
+    getHitCount("Y" + today.getFullYear() + "M" + today.getMonth() + "daily" + today.getDate(), "hits-daily");
 });
